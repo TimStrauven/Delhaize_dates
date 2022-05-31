512bf7a82e55601de0b8f9ae1eecbb04735049e7
Tim Strauven <tim.strauven@gmail.com>
2022-05-31 12:20:07 +0200

    Changed output for test_one

--- ocr.pytorch/test_one.py
+++ ocr.pytorch/test_one.py
@@ -3,14 +3,16 @@ from ocr import ocr
 import time
 import shutil
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from glob import glob
 import cv2
 
 def single_pic_proc(image_file):
-    image = np.array(Image.open(image_file).convert('RGB'))
+    image = Image.open(image_file).convert('RGB')
+    image = ImageOps.exif_transpose(image)
+    image = np.array(image)
     result, image_framed = ocr(image)
-    return result,image_framed
+    return result, image_framed
 
 def dis(image):
     cv2.imshow('image', image)
@@ -19,9 +21,11 @@ def dis(image):
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv)>=2:
+    if len(sys.argv) >= 2:
         filename = sys.argv[1]
         if filename.endswith('jpg') or filename.endswith('png'):
             result, image_framed = single_pic_proc(filename)
             print(result)
-            dis(image_framed)
+            for key in result:
+                print(result[key][1])
+            #dis(image_framed)
