15e91f17b82c368947f6715330a224039c79d9d3
TimStrauven <96440176+TimStrauven@users.noreply.github.com>
2022-06-01 15:55:16 +0200

    Update api_main.py

--- api_main.py
+++ api_main.py
@@ -71,6 +71,10 @@ def expiry_date() -> dict:
                 exp_date = extract_date(txt_out)
                 return {"prediction": exp_date}
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # You want to put the value of the env variable PORT if it exist (some services only open specifiques ports)
+    port = int(os.environ.get('PORT', 5000))
+    # Threaded option to enable multiple instances for
+    # multiple user access support
+    # You will also define the host to "0.0.0.0" because localhost will only be reachable from inside de server.
+    app.run(host="0.0.0.0", threaded=True, port=port)
