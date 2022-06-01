dac3bfdac0ff42d908b0ec7f14a160edd6e43246
nemishmehta <23.nemishmehta@gmail.com>
2022-06-01 12:10:29 +0200

    Updated regex.

--- ocr.pytorch/text_date_extraction.py
+++ ocr.pytorch/text_date_extraction.py
@@ -7,13 +7,13 @@ def extract_date(text):
     #pattern = "([0-9]{0,2})[/.-]*\s*([0-9]{2,4})[/.-]*\s*([0-9]{2,4})" 
 
     # Detect dd.mm.yyyy, dd.mm.yy, dd/mm/yyyy, dd/mm/yy, dd-mm-yyyy, dd-mm-yy  
-    pattern = "(([0-9]{2})?(\-|\.|\/)([0-9]{2})?(\-|\.|\/)20([0-9]{2})?)"
+    pattern = "(([0-9]{2})?(\-|\.|\/)([0-9]{2})?(\-|\.|\/)([0-9]{2,4})?)"
     match_string = re.search(pattern, text)
     #print(match_string)
 
     if match_string == None:
         # Detect mm.yyyy, mm.yy, mm/yy, mm/yyyy, mm-yy, mm-yyyy
-        pattern = "(([0-9]{2})(\-|\.|\/)20([0-9]{2})?)"
+        pattern = "(([0-9]{2})(\-|\.|\/)([0-9]{2,4})?)"
         match_string = re.search(pattern, text)
     
     if match_string == None:
